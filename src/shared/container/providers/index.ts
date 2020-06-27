aef116785fa5f8c26abf8919fd6ace9b69a491b5
milena-mognon <milenamognon@gmail.com>
2020-06-27 16:00:32 -0300

    added Mailtrap as a MailProvider option

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,7 @@ import { container } from 'tsyringe';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProviver';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import MailtrapProvider from './MailProvider/implementations/MailtrapProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -17,7 +18,14 @@ container.registerSingleton<IMailTemplateProvider>(
   HandlebarsMailTemplateProvider,
 );
 
+/**
+ * Mail Provider Options
+ */
+
+// const mailProvider = MailtrapProvider;
+const mailProvider = EtherealMailProvider;
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  container.resolve(mailProvider),
 );
